eb78c8dc00f57a59f1eb140e1f63488d7116c211
Tommy Rothman <tsr2121@columbia.edu>
2019-12-05 18:57:31 -0500

    create url file in tracking, and switch 2 url files (previously mixed up),

--- squirreltracker/squirreltracker/urls.py
+++ squirreltracker/squirreltracker/urls.py
@@ -13,12 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
+#from django.contrib import admin
+#from django.urls import path
+#from django.conf.urls import include
 
-urlpatterns = [
-    path('', include('tracking.urls')),
-    path('admin/', admin.site.urls),
+#urlpatterns = [
+    #path('', include('tracking.urls')),
+    #path('admin/', admin.site.urls),
 
-]
+#]
+
+
+    from django.urls import path
+
+    from . import views
+
+    urlpatters = [ 
+        path('', views.index, name='index'),
+    ]
---
+++ squirreltracker/tracking/urls.py
@@ -0,0 +1,13 @@
+
+
+
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+
+urlpatterns = [
+    path('', include('squirreltracker.urls')),
+    path('admin/', admin.site.urls),
+
+]
