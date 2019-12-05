1c10a0be962c8a845e2c3f3fa147a12dda7fce4d
Lara Vartanian <lv1997@gmail.com>
2019-12-05 15:39:34 -0500

    Complete Draft of Class Squirrel in models.py

--- squirreltracker/tracking/models.py
+++ squirreltracker/tracking/models.py
@@ -23,7 +23,7 @@ class Squirrel(models.Model):
 
 	Hectare = models.CharField(
 		help_text=_('Hectare'), 
-		max_length=10
+		max_length=10,
 		)
 
 	AM = 'AM'
@@ -41,11 +41,11 @@ class Squirrel(models.Model):
 		)
 
 	Date = models.DateField(
-		help_text=_('Date')
+		help_text=_('Date'),
 		)
 
 	Hectare_Squirrel_Number = models.PositiveIntegerField(
-		help_text=_('Hectare Squirrel Number')
+		help_text=_('Hectare Squirrel Number'),
 		)
 
 	JUVENILE = 'Juvenile'
@@ -89,17 +89,17 @@ class Squirrel(models.Model):
 
 	Highlight_Fur_Color = models.CharField(
 		help_text=_('Highlight Fur Color'), 
-		max_length=20
+		max_length=20,
 		)
 
 	Combination_of_Primary_and_Highlight_Color = models.CharField(
 		help_text=_('Primary and Highlight Color'), 
-		max_length=20 
+		max_length=20, 
 		)
 
 	Color_Notes = models.CharField(
 		help_text=_('Color Notes'), 
-		max_length=150
+		max_length=150,
 		)
 
 	GROUND_PLANE = 'Ground Plane' 
@@ -118,6 +118,122 @@ class Squirrel(models.Model):
 		max_length=25, 
 		choices=LOCATION_CHOICES, 
 		) 
+
+	Above_Ground_Sighter_Measurement = models.PositiveIntegerField(
+
+		)
+
+	Specific_Location = models.CharField(
+		help_text=_('Specific Location'), 
+		max_length=150,
+		)
+
+	Running = models.BooleanField(
+		help_text=_('Running'), 
+		)
+
+	Chasing = models.BooleanField(
+		help_text=_('Chasing'), 
+		)
+
+	Climbing = models.BooleanField(
+		help_text=_('Climbing'), 
+		)
+
+	Eating = models.BooleanField(
+		help_text=_('Eating'), 
+		)
+
+	Foraging = models.BooleanField(
+		help_text=_('Foraging'), 
+		)
+
+	Other_Activities = models.CharField(
+		help_text=_('Other Activities'), 
+		max_length=100,
+		)
+
+	Kuks = models.BooleanField(
+		help_text=_('Kuks'), 
+		)
+
+	Quaas = models.BooleanField(
+		help_text=_('Quaas'), 
+		)
+
+	Moans = models.BooleanField(
+		help_text=_('Moans'), 
+		)
+
+	Tail_flags = models.BooleanField(
+		help_text=_('Tail flags'), 
+		)
+
+	Tail_twitches = models.BooleanField(
+		help_text=_('Tail twitches'), 
+		)
+
+	Approaches = models.BooleanField(
+		help_text=_('Approaches'), 
+		)
+
+	Indifferent = models.BooleanField(
+		help_text=_('Indifferent'), 
+		)
+
+	Runs_from = models.BooleanField(
+		help_text=_('Runs from'), 
+		)
+
+	Approaches = models.BooleanField(
+		help_text=_('Approaches'), 
+		)
+
+	Other_Interactions = models.CharField(
+		help_text=_('Other Interactions'), 
+		max_length=100,
+		)
+
+	Lat_Long = models.CharField(
+		help_text=_('Lat Long'), 
+		max_length=100,
+		)
+
+
+	Zip_Codes = models.PositiveIntegerField(
+		help_text=_('Zip Codes'),
+		)
+
+	Community_Districts = models.PositiveIntegerField(
+		help_text=_('Community Districts'),
+		)
+
+	Borough_Boundaries = models.PositiveIntegerField(
+		help_text=_('Borough_Boundaries'),
+		)
+
+	City_Council_Districts = models.PositiveIntegerField(
+		help_text=_('City Council Districts'),
+		)
+
+	Police_Precincts = models.PositiveIntegerField(
+		help_text=_('Police Precincts'),
+		)
+
+ 	def __str__(self):
+ 		return self.name
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
