3d9fe815ca22cb3e77641d83678b6a926e01d122
Lara Vartanian <lv1997@gmail.com>
2019-12-05 18:13:56 -0500

    Filtered Fields to what we need

--- squirreltracker/tracking/models.py
+++ squirreltracker/tracking/models.py
@@ -17,21 +17,16 @@ class Squirrel(models.Model):
 	) 
 
 	Unique_Squirrel_ID = models.UUIDField(
-		id = models.UUIDField(primary_key=True), 
+	#	id = models.UUIDField(primary_key=True), 
 		help_text=_('Unique_ID'),
-	)
-
-	Hectare = models.CharField(
-		help_text=_('Hectare'), 
-		max_length=10,
 	)
 
 	AM = 'AM'
 	PM = 'PM'
 
 	SHIFT_CHOICES = (
-	        (AM, 'AM'),
-	        (PM, 'PM'),
+	    (AM, 'AM'),
+	    (PM, 'PM'),
 	)
 
 	Shift = models.CharField(
@@ -44,9 +39,6 @@ class Squirrel(models.Model):
 		help_text=_('Date'),
 	)
 
-	Hectare_Squirrel_Number = models.PositiveIntegerField(
-		help_text=_('Hectare Squirrel Number'),
-	)
 
 	JUVENILE = 'Juvenile'
 	ADULT = 'Adult'
@@ -87,21 +79,6 @@ class Squirrel(models.Model):
 	) 
 
 
-	Highlight_Fur_Color = models.CharField(
-		help_text=_('Highlight Fur Color'), 
-		max_length=20,
-	)
-
-	Combination_of_Primary_and_Highlight_Color = models.CharField(
-		help_text=_('Primary and Highlight Color'), 
-		max_length=20, 
-	)
-
-	Color_Notes = models.CharField(
-		help_text=_('Color Notes'), 
-		max_length=150,
-	)
-
 	GROUND_PLANE = 'Ground Plane' 
 	ABOVE_GROUND = 'Above Ground'
 	UNRECORDED_LOCATION = ''
@@ -119,9 +96,6 @@ class Squirrel(models.Model):
 		choices=LOCATION_CHOICES, 
 	) 
 
-	Above_Ground_Sighter_Measurement = models.PositiveIntegerField(
-
-	)
 
 	Specific_Location = models.CharField(
 		help_text=_('Specific Location'), 
@@ -185,46 +159,8 @@ class Squirrel(models.Model):
 		help_text=_('Runs from'), 
 	)
 
-	Approaches = models.BooleanField(
-		help_text=_('Approaches'), 
-	)
-
-	Other_Interactions = models.CharField(
-		help_text=_('Other Interactions'), 
-		max_length=100,
-	)
-
-	Lat_Long = models.CharField(
-		help_text=_('Lat Long'), 
-		max_length=100,
-	)
-
-
-	Zip_Codes = models.PositiveIntegerField(
-		help_text=_('Zip Codes'),
-	)
-
-	Community_Districts = models.PositiveIntegerField(
-		help_text=_('Community Districts'),
-	)
-
-	Borough_Boundaries = models.PositiveIntegerField(
-		help_text=_('Borough_Boundaries'),
-	)
-
-	City_Council_Districts = models.PositiveIntegerField(
-		help_text=_('City Council Districts'),
-	)
-
-	Police_Precincts = models.PositiveIntegerField(
-		help_text=_('Police Precincts'),
-	)
-
-	def __str__(self):
-        return self.name
-
-
-
+	
+	
 
 
 
