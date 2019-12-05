1447c30ae0ad6e80b9735bd344db14b09c51e5c3
Lara Vartanian <lv1997@gmail.com>
2019-12-04 20:04:12 -0500

    First sighting in views

--- squirreltracker/tracking/views.py
+++ squirreltracker/tracking/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def sightings_list(request, squirrel_id):
+	squirrels = Squirrel.objects.all()
+	context = {
+		'squirrels' : squirrels,
+	}
+	return render(request, 'tracking/all.html',  context)
