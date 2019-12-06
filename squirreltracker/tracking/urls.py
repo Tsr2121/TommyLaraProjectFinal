ae86589ccbc417e6657df9d6e7eb7cd59c34e6f8
amyyfory <xl2915@columbia.edu>
2019-12-06 18:09:15 -0500

    update urls.py

--- squirreltracker/tracking/urls.py
+++ squirreltracker/tracking/urls.py
@@ -2,9 +2,16 @@
 from django.urls import path
 from . import views
 
+#urlpatterns = [
+    #path('sightings/', views.sightings),
+    #path('squirrel_id', views.squirrel_id),
+    #path('add/', views.add_squirrel), 
+    #path('<unique-squirrel-id>/edit/', views.edit_squirrel),
+#]
+
 urlpatterns = [
     path('sightings/', views.sightings),
-    path('squirrel_id', views.squirrel_id),
-    path('add/', views.add_squirrel), 
-    path('<unique-squirrel-id>/edit/', views.edit_squirrel),
+    #path('squirrel_id', views.squirrel_id),
+    #path('add/', views.add_squirrel), 
+    path('<unique-squirrel-id>', views.edit_squirrel),
 ]
