3c46cc9192fd17d3506dbadd22e80b2000995ff7
Lara Vartanian <lv1997@gmail.com>
2019-12-05 21:09:22 -0500

    edit import.py to actually show under commands and change Date to Integer Field

--- squirreltracker/tracking/models.py
+++ squirreltracker/tracking/models.py
@@ -18,7 +18,8 @@ class Squirrel(models.Model):
 
 	Unique_Squirrel_ID = models.CharField(
 		help_text=_('Unique_ID'),
-		max_length=25
+		max_length=25, 
+		blank=True,
 	)
 
 	AM = 'AM'
@@ -33,9 +34,10 @@ class Squirrel(models.Model):
 		help_text=_('Shift Interval'), 
 		max_length=10, 
 		choices=SHIFT_CHOICES, 
+		blank=True,
 	)
 
-	Date = models.DateField(
+	Date = models.IntegerField(
 		help_text=_('Date'),
 	)
 
@@ -56,6 +58,7 @@ class Squirrel(models.Model):
 		help_text=_('Age Group'), 
 		max_length=20, 
 		choices=AGE_CHOICES, 
+		blank=True,
 	)
 
 
@@ -76,6 +79,7 @@ class Squirrel(models.Model):
 		help_text=_('Primary Fur Color'), 
 		max_length=10, 
 		choices=PRIMARY_FUR_COLOR_CHOICES, 
+		blank=True,
 	) 
 
 
@@ -94,69 +98,85 @@ class Squirrel(models.Model):
 		help_text=_('Location'), 
 		max_length=25, 
 		choices=LOCATION_CHOICES, 
+		blank=True,
 	) 
 
 
 	Specific_Location = models.CharField(
 		help_text=_('Specific Location'), 
 		max_length=150,
+		blank=True,
 	)
 
 	Running = models.BooleanField(
 		help_text=_('Running'), 
+		default=True,
 	)
 
 	Chasing = models.BooleanField(
 		help_text=_('Chasing'), 
+		default=True,
 	)
 
 	Climbing = models.BooleanField(
 		help_text=_('Climbing'), 
+		default=True,
 	)
 
 	Eating = models.BooleanField(
 		help_text=_('Eating'), 
+		default=True,
 	)
 
 	Foraging = models.BooleanField(
 		help_text=_('Foraging'), 
+		default=True,
 	)
 
 	Other_Activities = models.CharField(
 		help_text=_('Other Activities'), 
 		max_length=100,
+		blank=True,
 	)
 
 	Kuks = models.BooleanField(
 		help_text=_('Kuks'), 
+		default=True,
 	)
 
 	Quaas = models.BooleanField(
 		help_text=_('Quaas'), 
+		default=True,
 	)
 
 	Moans = models.BooleanField(
 		help_text=_('Moans'), 
+		default=True,
 	)
 
 	Tail_flags = models.BooleanField(
 		help_text=_('Tail flags'), 
+		default=True,
 	)
 
 	Tail_twitches = models.BooleanField(
 		help_text=_('Tail twitches'), 
+		default=True,
 	)
 
 	Approaches = models.BooleanField(
 		help_text=_('Approaches'), 
+		default=True,
 	)
 
 	Indifferent = models.BooleanField(
 		help_text=_('Indifferent'), 
+		default=True,
 	)
 
 	Runs_from = models.BooleanField(
 		help_text=_('Runs from'), 
+		default=True,
 	)
 
 	
