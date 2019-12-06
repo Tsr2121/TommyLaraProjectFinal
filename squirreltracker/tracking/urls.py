0abb589bc071972db23b892b6554df2bd8dc8188
Lara Vartanian <lv1997@gmail.com>
2019-12-06 15:01:12 -0500

    create and add code in views.py, urls.py, and forms.py

--- squirreltracker/tracking/urls.py
+++ squirreltracker/tracking/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('sightings/', views.sightings),
+    path('squirrel_id', views.squirrel_id),
+    path('add/', views.add_squirrel), 
+    path('<unique-squirrel-id>/edit/', views.edit_squirrel),
 ]
--- squirreltracker/tracking/views.py
+++ squirreltracker/tracking/views.py
@@ -10,3 +10,49 @@ def sightings(request):
 		'squirrels': squirrels,
 	}
 	return render(request, 'sightings.html', context)
+
+
+def squirrel_id(request, Unique_Squirrel_ID ):
+	squirrel = Squirrel.objects.get(id=Unique_Squirrel_ID)
+	return HttpResponse(squirrel.Unique_Squirrel_ID)
+	return render(request, 'squirrel_id.html')
+
+
+def add_squirrel(request):
+	if request.method == 'POST':
+		form = SquirrelForm(request.POST)
+		# check data with form 
+		if form.is_valid():
+			form.save()
+			return redirect(f'/tracking/list')
+
+	else:
+		form = SquirrelForm()
+
+	context = {
+		'form': form, 
+		'jazz': True,
+	}
+
+	return render(request, 'tracking/edit.html', context)
+
+
+def edit_squirrel(request, squirrel_id):
+	squirrel = Squirrel.objects.get(id=Unique_Squirrel_ID)
+	if request.methof == 'POST':
+		form = SquirrelForm(request.POST, instance=squirrel)
+
+		if form.is_valid():
+			form.save()
+			return redirect(f'/tracking/{Unique_Squirrel_ID}')
+
+	else:
+		form = SquirrelForm(instance=squirrel)
+
+	context = {
+		'form': form, 
+	}
+
+	return render(request, 'tracking/edit.html', context)
+
+
