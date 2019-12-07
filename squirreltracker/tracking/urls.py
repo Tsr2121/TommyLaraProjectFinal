b74383b533ad419cc1e7894d91d7b37b2ff96c40
amyyfory <xl2915@columbia.edu>
2019-12-06 21:13:25 -0500

    updates

--- squirreltracker/tracking/urls.py
+++ squirreltracker/tracking/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('sightings/', views.sightings),
     #path('squirrel_id', views.squirrel_id),
     #path('add/', views.add_squirrel), 
-    path('<str:unique-squirrel-id>', views.edit_squirrel),
+    path('<str:unique-squirrel-id>/edit/', views.edit_squirrel),
 ]
