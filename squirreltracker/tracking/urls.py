0761a25e6b418f825b4a8a28b088069b1e11e86e
amyyfory <xl2915@columbia.edu>
2019-12-07 10:12:35 -0500

    final view.edit

--- squirreltracker/tracking/urls.py
+++ squirreltracker/tracking/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('sightings/', views.sightings, name='sightings_list'),
     #path('squirrel_id', views.squirrel_id),
-    path('sightings/add/', views.add_squirrel, name='add_sightings'), 
+    path('sightings/add', views.add_squirrel, name='add_sightings'), 
     path('sightings/<str:Unique_Squirrel_ID>', views.edit_squirrel, name='edit_sightings'),
 ]
 
