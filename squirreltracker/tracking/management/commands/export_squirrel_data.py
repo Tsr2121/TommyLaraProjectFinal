97fcb3647edcddbac4f65659920c99ec8ef9a02f
Lara Vartanian <lv1997@gmail.com>
2019-12-08 00:34:21 -0500

    edit export.py

--- squirreltracker/tracking/management/commands/export_squirrel_data.py
+++ squirreltracker/tracking/management/commands/export_squirrel_data.py
@@ -1,47 +1,27 @@
-import csv
 
-from django.core.management.base import BaseCommand
+import csv
+import sys
+
+from django.core.management.base import BaseCommand, CommandError
 
 from tracking.models import Squirrel
 
-import pandas as pd 
 
 
 class Command(BaseCommand):
-    help = 'export fields'
-
     def add_arguments(self, parser):
         parser.add_argument('csv_file')
 
     def handle(self, *args, **options):
-        with open(options['csv_file']) as fp:
-            writer = csv.writer(fp)
-            data = list(reader)
+        meta = Squirrel._meta
+        fields = [j.name for j in meta.fields]
+        csv_file = options['csv_file']
+        
+        print(csv_file)
+        with open(csv_file,'w') as csvfile:
+            w = csv.writer(csvfile)
 
-        for item in data:
-            p = Squirrel(
-                X=item['X'],
-                Y=item['Y'],
-                Unique_Squirrel_ID=item['Unique Squirrel ID'],
-                Shift = item['Shift'], 
-                Date = item['Date'], 
-                Age = item['Age'], 
-                Primary_Fur_Color = item['Primary Fur Color'], 
-                Location = item['Location'],  
-                Specific_Location = item['Specific Location'], 
-                Running = item['Running'], 
-                Chasing = item['Chasing'], 
-                Climbing = item['Climbing'], 
-                Eating = item['Eating'], 
-                Foraging = item['Foraging'], 
-                Other_Activities = item['Other Activities'], 
-                Kuks = item['Kuks'], 
-                Quaas = item['Quaas'], 
-                Moans = item['Moans'], 
-                Tail_flags = item['Tail flags'], 
-                Tail_twitches = item['Tail twitches'], 
-                Approaches = item['Approaches'], 
-                Indifferent = item['Indifferent'], 
-                Runs_from = item['Runs from'], 
-            )
-            p.to_csv(csv_file)
+            w.writerow(fields)
+            for i in Squirrel.objects.all():
+                w.writerow([getattr(i, field) for field in fields])
+
